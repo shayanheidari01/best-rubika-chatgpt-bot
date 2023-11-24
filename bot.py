64458e49914bb4e2927966e687c3637e681d846a
Shayan Heidari <119259115+shayanheidari01@users.noreply.github.com>
2023-11-25 02:49:46 +0330

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 from rubpy import Client, handlers
 from rubpy.structs import Struct
-from typing import NoReturn, Literal
 from httpx import AsyncClient, ReadTimeout
 from asyncio import run, create_task, sleep as aiosleep
 
@@ -9,21 +8,17 @@ response_queue: list = []
 http_client = AsyncClient()
 
 # List of group IDs to filter messages from
-groups = []
+groups = ['g0CytnM03bdc3d548fac7e6e42fc0507']
 
-async def message_updates_model(message: Struct, result) -> (Literal[True] | None):
+async def message_updates_model(message: Struct, result):
     # Custom model to handle message updates for User messages
-    if message.action == 'New' and message.type == 'User':
-        return True
-    return None
+    return message.action == 'New' and message.type == 'User'
 
-async def message_updates_group_model(message: Struct, result) -> (Literal[True] | None):
+async def message_updates_group_model(message: Struct, result):
     # Custom model to handle message updates for Group messages in specified groups
-    if message.action == 'New' and message.type == 'Group' and message.object_guid in groups:
-        return True
-    return None
+    return message.action == 'New' and message.type == 'Group' and message.object_guid in groups
 
-async def chooser() -> NoReturn:
+async def chooser():
     while True:
         await aiosleep(0.1)
         for queue in response_queue:
@@ -37,29 +32,18 @@ async def chooser() -> NoReturn:
 
 async def send_chatgpt_request(http: AsyncClient, text: str):
     # HTTP request to a ChatGPT service for text generation
-    headers = {
-        'Host': 'us-central1-chat-for-chatgpt.cloudfunctions.net',
-        'Connection': 'keep-alive',
-        'Accept': '*/*',
-        'User-Agent': 'com.tappz.aichat/1.2.2 iPhone/16.3.1 hw/iPhone12_5',
-        'Accept-Language': 'en',
-        'Content-Type': 'application/json; charset=UTF-8',
-    }
-    endpoint = 'https://us-central1-chat-for-chatgpt.cloudfunctions.net/basicUserRequestBeta'
-
-    data = {
-        'data': {
-            'message': text,
-        },
-    }
+    endpoint = 'https://chatgpt-api3.onrender.com'
+    data = {'text': text}
 
     for trying in range(3):
         try:
-            response = await http.post(endpoint, json=data, headers=headers)
+            response = await http.post(endpoint, json=data)
             response_data = response.json()
-            return response_data['result']['choices'][0]['text']
+            return response_data.get('message')
         except (TimeoutError, ReadTimeout):
             continue
+        except Exception as exc:
+            print('ERROR:', exc)
 
     return 'خطا در دریافت پاسخ!'
 
@@ -67,6 +51,9 @@ async def reply_to_user(client: Client, object_guid: str, text: str, message_id:
     # Generate a ChatGPT response and send it as a reply to the user
     chatgpt_response = await send_chatgpt_request(http_client, text)
     if isinstance(chatgpt_response, str):
+        if object_guid[0] == 'g':
+            return await client.send_message(object_guid, '**● پاسخ چت‌جی‌پی‌تی:**\n' + chatgpt_response, message_id)
+
         chatgpt_response += '\n\n**توجه: به دلیل شلوغ بودن ربات برای پاسخ به شما، شما را صف نگه می‌دارد!**\n\n**لطفا جهت حمایت ما در کانال های زیر عضو شوید:**\n@shython\n@SiSiGIFS\n**گروه ما:**\nhttps://rubika.ir/joing/EBCIHEDF0CXPIXETEMOLSMUCOUCBBKIA'
         try:
             return await client.send_message(object_guid, '**● پاسخ چت‌جی‌پی‌تی:**\n' + chatgpt_response, message_id)
@@ -90,16 +77,16 @@ async def handler_message_group_updates(client: Client, update: Struct):
         if text.startswith('//'):
             response_queue.append(reply_to_user(client, object_guid, text[2:].strip(), message_id))
 
-async def main() -> NoReturn:
+async def main():
     create_task(chooser())
 
     async with Client('bot') as client:
         @client.on(handlers.MessageUpdates(message_updates_model))
-        async def updates(update: Struct) -> NoReturn:
+        async def updates(update: Struct):
             create_task(handler_message_updates(client, update))
 
         @client.on(handlers.MessageUpdates(message_updates_group_model))
-        async def updates(update: Struct) -> NoReturn:
+        async def updates(update: Struct):
             create_task(handler_message_group_updates(client, update))
 
         await client.run_until_disconnected()
